fe1c0af821450bf1de32aa54da22e607bf004474
Emmanuel Monsalve <emonsalve@buk.co>
2023-02-28 15:31:00 -0500

    feat: Agregamos tarea de plobar he imprimir un vector de manera aleatoria

--- ImprimirVector/ImprimirVector/Program.cs
+++ ImprimirVector/ImprimirVector/Program.cs
@@ -6,7 +6,29 @@ namespace ImprimirVector
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Pedimos al usuario el tamaño del array
+            Console.WriteLine("Ingrese el tamaño del array:");
+            int tamaño = Convert.ToInt32(Console.ReadLine());
+
+            // Definimos un array de enteros con el tamaño ingresado por el usuario
+            int[] miVector = new int[tamaño];
+
+            // Llenamos el array con valores aleatorios entre 0 y 100
+            Random rnd = new Random();
+            for (int i = 0; i < tamaño; i++)
+            {
+                miVector[i] = rnd.Next(1, (tamaño + 1)); // Genera un número aleatorio entre 0 y 100
+            }
+
+            // Imprime el arreglo generado
+            Console.WriteLine("El arreglo generado es:");
+            foreach (int num in miVector)
+            {
+                Console.Write(num + " ");
+            }
+
+            // Esperamos a que el usuario presione una tecla para salir
+            Console.ReadKey();
         }
     }
 }
