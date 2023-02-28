dd19a1837b5efaaa8e98816a0c1f260a211016c0
Emmanuel Monsalve <emonsalve@buk.co>
2023-02-28 15:35:06 -0500

    refactor: se refactoriza el código a metodos mas comunes en c#

--- ImprimirVector/ImprimirVector/Program.cs
+++ ImprimirVector/ImprimirVector/Program.cs
@@ -8,16 +8,16 @@ namespace ImprimirVector
         {
             // Pedimos al usuario el tamaño del array
             Console.WriteLine("Ingrese el tamaño del array:");
-            int tamaño = Convert.ToInt32(Console.ReadLine());
+            int tamaño = int.Parse(Console.ReadLine());
 
             // Definimos un array de enteros con el tamaño ingresado por el usuario
             int[] miVector = new int[tamaño];
 
             // Llenamos el array con valores aleatorios entre 0 y 100
-            Random rnd = new Random();
+            Random random = new Random();
             for (int i = 0; i < tamaño; i++)
             {
-                miVector[i] = rnd.Next(1, (tamaño + 1)); // Genera un número aleatorio entre 0 y 100
+                miVector[i] = random.Next(1, (tamaño + 1)); // Genera un número aleatorio entre 0 y 100
             }
 
             // Imprime el arreglo generado
