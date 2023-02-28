4b2c8011a58f316be34f721d2173727e0089d94f
Emmanuel Monsalve <emonsalve@buk.co>
2023-02-27 21:26:16 -0500

    feat: agregar carpetas faltantes

--- BubleSort/BubleSort/Program.cs
+++ BubleSort/BubleSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace BubleSort
 {
@@ -6,14 +7,47 @@ namespace BubleSort
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 5, 2, 7, 3, 1, 8, 6, 9, 4 };
+            // Pide al usuario que ingrese la cantidad de elementos del arreglo
+            Console.Write("Ingrese la cantidad de elementos del arreglo: ");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Arreglo antes de ser ordenado:");
+            // Crea un arreglo de n posiciones
+            int[] arr = new int[n];
+
+            // Llena el arreglo con números aleatorios entre 1 y 100
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = random.Next(1, (n + 1));
+            }
+
+            // Imprime el arreglo generado
+            Console.WriteLine("El arreglo generado es:");
             foreach (int num in arr)
             {
                 Console.Write(num + " ");
             }
 
+            // Mide el tiempo de ejecución del algoritmo de ordenamiento
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            BubbleSort(arr);
+            stopwatch.Stop();
+
+            // Imprime el arreglo ordenado
+            Console.WriteLine("\nEl arreglo ordenado es:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+
+            // Imprime el tiempo de ejecución en microsegundos
+            Console.WriteLine($"\nTiempo de ejecución: {stopwatch.ElapsedTicks / (TimeSpan.TicksPerMillisecond / 1000)} μs");
+
+            Console.ReadLine();
+        }
+
+        static void BubbleSort(int[] arr)
+        {
             int n = arr.Length;
             for (int i = 0; i < n - 1; i++)
             {
@@ -27,14 +61,7 @@ namespace BubleSort
                     }
                 }
             }
-
-            Console.WriteLine("\nArreglo ordenado:");
-            foreach (int num in arr)
-            {
-                Console.Write(num + " ");
-            }
-
-            Console.ReadLine();
         }
     }
+
 }
---
+++ ImprimirVector/ImprimirVector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImprimirVector
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
